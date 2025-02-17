fb9c095e8e71abe25c5feb0d5443dc613e704feb
Indranil Banerjee <banerjeeindranil854@gmail.com>
2025-02-17 10:35:19 +0000

    added for the code changes

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Socket, Server } = require("socket.io");
+
+const  server  = new Server({cors: {origin: "http://localhost:4200"}});
+
+server.on("connection", (socket) => {
+    console.log("connected");
+    socket.on("message", (data) => {
+        console.log(data);
+        socket.broadcast.emit("received", {data: data,message:"data send"});
+    });
+});
+
+server.listen(4000);
+
